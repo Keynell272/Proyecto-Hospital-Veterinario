4885354569e3d21cd0008a7e7df833c1d2b5a1d3
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-15 18:08:42 -0600

    Update Especialidad.cpp

--- Especialidad.cpp
+++ Especialidad.cpp
@@ -1,3 +1,14 @@
 #include "Especialidad.h"
 
+Especialidad::Especialidad(): especialidad(""){}
+Especialidad::Especialidad(string esp): especialidad(esp){}
+Especialidad::~Especialidades() {}
 
+void Especialidad::setEspecialidad(string esp) { especialidad = esp; }
+string Especialidad::getEspecialidad() { return especialidad; }
+
+string Especialidad::toString(){
+  stringstream s;
+  s << "Especialidad: " << especialidad << endl;
+  return s.str();
+}
