12aca708dfe5c07632ffef9fda2109d0631d911a
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-15 18:25:42 -0600

    Create Doctor.cpp

---
+++ Doctor.cpp
@@ -0,0 +1,22 @@
+#include "Doctor.h"
+
+Doctor::Doctor(): nombre(""), especialidad(NULL) {}
+Doctor::Doctor(string nom, Especialidad& esp): nombre(nom){
+	especialidad = new Especialidad(esp);
+}
+Doctor::~Doctor() { delete especialidad; }
+
+void Doctor::setNombre(string nom) { nombre = nom; }
+void Doctor::setEspecialidad(Especialidad& esp) { 
+	delete especialidad;
+	especialidad = new Especialidad(esp);
+}
+
+string Doctor::getNombre() { return nombre; }
+Especialidad* Doctor::getEspecialidad() { return especialidad; }
+
+string Doctor::toString(){
+	stringstream s;
+	s << "Doctor " << nombre << " \n" << especialidad->toString() << endl;
+	return s.str();
+}
