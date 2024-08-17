746a8fd9a311fc0e6d44a903d47ec4148f6c1635
latrodectusVal <valpe0320@gmail.com>
2024-08-17 17:37:36 -0600

    Update Mascotas.cpp

--- Mascotas.cpp
+++ Mascotas.cpp
@@ -1,1 +1,21 @@
+#include "Mascotas.h"
 
+Mascota::Doctor(): nombre(""), especialidad(NULL) {}
+Doctor::Doctor(string nom, Especialidad& esp): nombre(nom){
+	especialidad = new Especialidad(esp);
+}
+Doctor::~Doctor() { delete especialidad; }
+
+void Doctor::setNombre(string nom) { nombre = nom; }
+void Doctor::setEspecialidad(Especialidad& esp) { 
+	delete especialidad;
+	especialidad = new Especialidad(esp);
+}
+
+string Doctor::getNombre() { return nombre; }
+Especialidad* Doctor::getEspecialidad() { return especialidad; }
+
+string Doctor::toString(){
+	stringstream s;
+	s << "Doctor " << nombre << " \n" << especialidad->toString() << endl;
+	return s.str();
