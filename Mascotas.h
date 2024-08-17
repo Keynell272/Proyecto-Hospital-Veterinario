79a152b3076340197d15a344df46871a0f1bce6d
latrodectusVal <valpe0320@gmail.com>
2024-08-17 17:24:03 -0600

    Update Mascotas.h

--- Mascotas.h
+++ Mascotas.h
@@ -1,1 +1,16 @@
+#ifndef MASCOTA_H
+#define MASCOTA_H
+
+#include "Dueno.h"
+
+class Mascota {
+private:
+    string nombre;
+    Dueno* dueno;
+
+public:
+    Mascota(string, Duenio*);
+};
+
+#endif // MASCOTA_H
 
