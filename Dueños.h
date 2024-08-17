a6a96e245edd3fc20faf906979e5c15e4d950baa
latrodectusVal <valpe0320@gmail.com>
2024-08-17 16:43:37 -0600

    Update Dueños.h

--- Dueños.h
+++ Dueños.h
@@ -1,1 +1,18 @@
+#ifndef DUENO_H
+#define DUENO_H
+
+#include <iostream>
+using namespace std;
+
+class Dueno {
+private:
+  string nombre;
+public:
+Dueno();
+Dueno(string n)
+
+};
+   
+
+#endif // DUENO_H
 
