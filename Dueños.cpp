27669f01250689f673b1e4c5c37bf4fe111c4a45
latrodectusVal <valpe0320@gmail.com>
2024-08-17 17:08:06 -0600

    Update Dueños.cpp

--- Dueños.cpp
+++ Dueños.cpp
@@ -10,4 +10,27 @@ Dueno::Dueno(){
   ID = 0;
 }
 
+Dueno::~Dueno() {}
 
+void Dueno::setNombre(string nom) { 
+  nombre = nom; 
+}
+
+void Dueno::setID(int identidad){
+  ID = identidad;
+}
+
+string Dueno::getNombre() {
+  return nombre; 
+}
+
+int Dueno::getID(){
+  return ID;
+}
+
+string Dueno::toString(){
+  stringstream s;
+  s << "Nombre del dueno: " << nombre << endl;
+  s<< "Numero de identidad: "<< ID<<endl;
+  return s.str();
+}
