96564b3252998435285a7570f0b36d2b413c4db8
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-19 19:26:32 -0600

    Update vEspecialidades.h

--- vEspecialidades.h
+++ vEspecialidades.h
@@ -1,3 +1,5 @@
+#ifndef VESPECIALIDADES_H
+#define VESPECIALIDADES_H
 #pragma once
 #include "Especialidad.h"
 
@@ -9,7 +11,9 @@ public:
 	vEspecialidades();
 	~vEspecialidades();
 
-	void agregarEspecialidad(Especialidad&);
+	void agregarEspecialidad(Especialidad*);
 	void imprimeContenedor();
 	void eliminarEspecialidadPorNombre(string);
 };
+
+#endif // VESPECIALIDADES_H
