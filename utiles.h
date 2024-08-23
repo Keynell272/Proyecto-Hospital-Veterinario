baf3e31051473bc173d45c54223ee2926fa2f176
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-23 14:52:39 -0600

    Create utiles.h

---
+++ utiles.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Especialidad.h"
+
+#ifndef UTILES_H
+#define UTILES_H
+
+void imprimeCadena(string);
+void limpiaPantalla();
+string leerCadena();
+int leerEntero();
+void esperandoEnter();
+
+#endif
