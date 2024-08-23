a21909aebac65427929d76967e31ba86a0595047
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-23 14:56:43 -0600

    Update Hospital.cpp

--- Hospital.cpp
+++ Hospital.cpp
@@ -1,1 +1,48 @@
+#include "Hospital.h"
+#include "utiles.h"
 
+void Hospital::ingresarEspecialidad() {
+	string esp;
+	Especialidad* espe;
+	char res = ' ';
+	
+	while (res != 'n') {
+		imprimeCadena("Ingresando especialidad.\n\n");
+		imprimeCadena("Escriba el nombre de la especialidad: ");cin >> esp;
+
+		espe = new Especialidad(esp);
+
+		vecEspecialidades->agregarEspecialidad(espe);
+
+		imprimeCadena("Desea ingresar otra especialidad (s/n): ");cin >> res;
+	}
+
+}
+
+void Hospital::ingresarDoctor() {
+
+}
+
+void Hospital::ingresarDueno() {
+
+}
+
+void Hospital::ingresarMascota() {
+
+}
+
+
+void Hospital::menuPrincipal() {
+
+
+	
+}
+void Hospital::menuAdministrativo() {
+
+}
+void Hospital::menuCitas() {
+
+}
+void Hospital::menuBusquedasListados() {
+
+}
