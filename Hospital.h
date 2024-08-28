889f3860f9037b3462757f131a498ea060964f3c
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-28 08:14:18 -0600

    Update Hospital.h

--- Hospital.h
+++ Hospital.h
@@ -13,15 +13,29 @@ private:
 	vDuenos* vecDuenos;
 	vMascotas* vecMascotas;
 public:
+	Hospital();
+	~Hospital();
+
+	void menuOpcion();
+	void manejarSubMenu(int);           
+	void manejarOpcionAdministrativo(int);  
+	void manejarOpcionCitas(int);         
+	void manejarOpcionBusquedas(int);
+	
+	void menuPrincipal();
+	void menuAdministrativo();
+	void menuCitas();
+	void menuBusquedasListados();
+	
 	void ingresarEspecialidad();
 	void ingresarDoctor();
 	void ingresarDueno();
 	void ingresarMascota();
 
-	void menuPrincipal();
-	void menuAdministrativo();
-	void menuCitas();
-	void menuBusquedasListados();
+	void mostrarDoctor();
+	void mostrarEspecialidades();
+	void mostrarDuenos();
+	void mostrarMascotas();
 };
 
 #endif // !HOSPITAL_H
