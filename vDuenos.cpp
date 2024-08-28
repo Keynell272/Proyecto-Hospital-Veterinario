8d1ae9539bc6f8e29cd59c76c555a9e0f875d353
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-28 08:05:53 -0600

    Update vDuenos.cpp

--- vDuenos.cpp
+++ vDuenos.cpp
@@ -1,8 +1,8 @@
 #include "vDuenos.h"
 
-vDuenos::vDuenos(): cantidad(0), tamano(100) {
-	vec = new Dueno * [100];
-	for (int i = 0; i < 100; i++) {
+vDuenos::vDuenos(int n): cantidad(0), tamano(n) {
+	vec = new Dueno * [n];
+	for (int i = 0; i < n; i++) {
 		vec[i] = nullptr;
 	}
 }
@@ -10,7 +10,25 @@ vDuenos::~vDuenos() {
 	for (int i = 0; i < cantidad; i++) {
 		delete vec[i];
 	}
-	delete[]vec;
+	delete[] vec;
+}
+
+void vDuenos::setCantidad(int cant) {
+	cantidad = cant;
+}
+int vDuenos::getCantidad() {
+	return cantidad;
+}
+Dueno* vDuenos::getVec(int n) {
+	// Convertimos n a índice de array (0-indexed)
+	n = n - 1;
+
+	if (n >= 0 && n < cantidad) {
+		return vec[n];
+	}
+
+	imprimeCadena("Dueno no encontrado.\n");
+	return nullptr;
 }
 
 void vDuenos::agregarDueno(Dueno* d) {
@@ -25,7 +43,8 @@ void vDuenos::agregarDueno(Dueno* d) {
 
 void vDuenos::imprimeContenedor() {
 	for (int i = 0; i < cantidad; i++) {
-		cout << vec[i]->toString() << endl;
+		imprimeCadena("--->");imprimeEntero(i + 1);imprimeCadena("\n");
+		imprimeCadena(vec[i]->toString());imprimeCadena("\n");
 	}
 }
 
@@ -33,11 +52,13 @@ void vDuenos::eliminarDuenoPorNombre(string nom) {
 	for (int i = 0; i < cantidad; i++) {
 		if (vec[i]->getNombre() == nom) {
 			delete vec[i];
+			vec[i] = nullptr;
 			for (int j = i; j < cantidad - 1; ++j) {
 				vec[j] = vec[j + 1];
 			}
 			vec[cantidad - 1] = nullptr;
 			cantidad--;
+			return;
 		}
 		else {
 			cout << "Dueno no encontrado." << endl;
