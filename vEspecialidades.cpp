531d1cbc97ed85ebb782b645f5ccbc4d1b0d95b0
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-28 07:58:18 -0600

    Update vEspecialidades.cpp

--- vEspecialidades.cpp
+++ vEspecialidades.cpp
@@ -13,8 +13,26 @@ vEspecialidades::~vEspecialidades() {
 	delete[]vec;
 }
 
+void vEspecialidades::setCantidad(int cant) {
+	cantidad = cant;
+}
+int vEspecialidades::getCantidad() {
+	return cantidad;
+}
+Especialidad* vEspecialidades::getVec(int n) {
+	// Convertimos n a índice de array (0-indexed)
+	n = n - 1;
+	
+	if (n >= 0 && n < cantidad) {
+		return vec[n];
+	}
+
+	imprimeCadena("Especialidad no encontrada.\n");
+	return nullptr;
+}
+
 void vEspecialidades::agregarEspecialidad(Especialidad* esp) {
-	if (cantidad < 100) {
+	if (cantidad < tamano) {
 		vec[cantidad] = esp;
 		cantidad++;
 	}
@@ -25,7 +43,8 @@ void vEspecialidades::agregarEspecialidad(Especialidad* esp) {
 
 void vEspecialidades::imprimeContenedor() {
 	for (int i = 0; i < cantidad; i++) {
-		cout << vec[i]->toString() << endl;
+		imprimeCadena("--->");imprimeEntero(i + 1);imprimeCadena("\n");
+		imprimeCadena(vec[i]->toString());imprimeCadena("\n");
 	}
 }
 
@@ -33,15 +52,26 @@ void vEspecialidades::eliminarEspecialidadPorNombre(string nom) {
 	for (int i = 0; i < cantidad; i++) {
 		if (vec[i]->getEspecialidad() == nom) {
 			delete vec[i];
+			vec[i] = nullptr;
 			for (int j = i; j < cantidad - 1; ++j) {
 				vec[j] = vec[j + 1];
 			}
 			vec[cantidad - 1] = nullptr;
 			cantidad--;
+			return;
 		}
 		else {
 			cout << "Especialidad no encontrada." << endl;
 		}
 	}
 
+}
+
+bool vEspecialidades::estaVacio() {
+	if (cantidad == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
