aa3447513761e1ddb8b4e387946555f45c759527
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-08-28 07:21:32 -0600

    Update utiles.h

--- utiles.h
+++ utiles.h
@@ -1,13 +1,23 @@
 #pragma once
-#include "Especialidad.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
 
 #ifndef UTILES_H
 #define UTILES_H
 
 void imprimeCadena(string);
+void imprimeEntero(int);
 void limpiaPantalla();
 string leerCadena();
 int leerEntero();
 void esperandoEnter();
+bool respuestaValida(int);
 
 #endif
