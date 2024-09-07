a969e1d6fe641427fdef03240d7c8fd7d3ac798c
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-09-07 11:49:40 -0600

    Create Horario.cpp

---
+++ Horario.cpp
@@ -0,0 +1,29 @@
+#include "Horario.h"
+
+Horario::Horario(): dia(""){}
+Horario::Horario(string _dia) : dia(_dia) {
+	int y = 8;
+	for (int i = 0; i < tam; i++) {
+		hora[i] = new Hora(to_string(y), "00");
+		y++;
+	}
+}
+Horario::~Horario() {
+	for (int i = 0; i < tam; i++) {
+		delete hora[i];
+	}
+}
+
+string Horario::getDia() { return dia; }
+
+void Horario::setDia(string _dia) { dia = _dia; }
+
+string Horario::toString() {
+	stringstream s;
+	s << "Dia: " << dia << "\n";
+	for (int i = 0; i < tam; i++) {
+		s <<"->"<<i<<"\n"
+			<< "   " << hora[i]->toString() << "\n";
+	}
+	return s.str();
+}
