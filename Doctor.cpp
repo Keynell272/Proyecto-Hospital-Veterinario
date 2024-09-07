6df76440ae3a11ce4fcd7fd4f8bdddf934b9613f
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-09-07 11:48:12 -0600

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -1,8 +1,19 @@
 #include "Doctor.h"
 
 Doctor::Doctor(): nombre(""), especialidad(nullptr) {}
-Doctor::Doctor(string nom, Especialidad* esp): nombre(nom), especialidad(esp) {}
-Doctor::~Doctor() {}
+Doctor::Doctor(string nom, Especialidad* esp): nombre(nom), especialidad(esp) {
+	horario[0] = new Horario("Lunes");
+	horario[1] = new Horario("Martes");
+	horario[2] = new Horario("Miercoles");
+	horario[3] = new Horario("Jueves");
+	horario[4] = new Horario("Viernes");
+	horario[5] = new Horario("Sabado");
+}
+Doctor::~Doctor() {
+	for (int i = 0; i < tam2; i++) {
+		delete horario[i];
+	}
+}
 
 void Doctor::setNombre(string nom) { nombre = nom; }
 void Doctor::setEspecialidad(Especialidad* esp) { 
