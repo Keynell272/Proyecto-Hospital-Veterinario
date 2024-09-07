c83b2d34024e9121acdefaafd6caec9f51427e96
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-09-07 11:44:39 -0600

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -2,11 +2,15 @@
 #define DOCTOR_H
 #pragma once
 #include "Especialidad.h"
+#include "Horario.h"
+
+const int tam2 = 6;
 
 class Doctor {
 private:
 	string nombre;
 	Especialidad* especialidad;
+	Horario * horario[tam2];
 public:
 	Doctor();
 	Doctor(string, Especialidad*);
