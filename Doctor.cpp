f5f270f1ea09d32e37797489af9a0ebc6f4750f3
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-09-10 20:18:41 -0600

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -28,3 +28,27 @@ string Doctor::toString(){
 	s << "Doctor " << nombre << " \n" << especialidad->toString() << endl;
 	return s.str();
 }
+
+string Doctor::toStringHorarios() {
+	//horario[4]->getHora(1)->setEscogida(true);
+	//horario[1]->getHora(10)->setEscogida(true);
+	stringstream s;
+	s << "\n\tHorarios\n" << endl;
+	for (int i = 0; i < 6; i++) {
+		s << horario[i]->getDia() << " ";
+	}
+	s << "\n";
+	for (int i = 0; i < tam; i++) {
+		for (int j = 0; j < 6; j++) {
+			if(horario[j]->getHora(i)->getEscogida() == false) {
+				s << horario[j]->getHoraString(i) << "\t";
+			}
+			else {
+				s << "ocupado" << "\t";
+			}
+		}
+		s << "\n";
+	}
+	
+	return s.str();
+}
