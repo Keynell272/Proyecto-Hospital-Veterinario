70196687344e6e0faf9af0cac051a32b44f05cdd
Keynell272 <126612065+Keynell272@users.noreply.github.com>
2024-09-24 11:15:33 -0600

    Update Cita.cpp

--- Cita.cpp
+++ Cita.cpp
@@ -1,22 +1,48 @@
 #include "Cita.h"
 
-Cita::Cita(): mas(nullptr), doc(nullptr), codigo(0)
+Cita::Cita(): mas(nullptr), doc(nullptr)
 {}
-Cita::Cita(Mascota* _mas, Doctor* _doc, int cod) : mas(_mas), doc(_doc), codigo(cod)
+Cita::Cita(Mascota* _mas, Doctor* _doc, int _dia, int _hora) : mas(_mas), doc(_doc), dia(_dia), hora(_hora)
 {}
 Cita::~Cita() {}
 
-void Cita::setCodigo(int n) { codigo = n; }
 void Cita::setMascota(Mascota* m) { mas = m; }
 void Cita::setDoctor(Doctor* d) { doc = d; }
 
-int Cita::getCodigo() { return codigo; }
 Mascota* Cita::getMascota() { return mas; }
 Doctor* Cita::getDoctor() { return doc; }
-
+int Cita::getDia() { return dia; }
+int Cita::getHora() { return hora; }
 
 string Cita::toString() {
 	stringstream s;
-	s << "\nCita.\n";
+	s << "\nCita.\n"
+		<< "Dia: ";
+	switch (dia)
+	{
+	case 0:
+		s << "Lunes" << endl;
+		break;
+	case 1:
+		s << "Martes" << endl;
+		break;
+	case 2:
+		s << "Miercoles" << endl;
+		break;
+	case 3:
+		s << "Jueves" << endl;
+		break;
+	case 4:
+		s << "Viernes" << endl;
+		break;
+	case 5:
+		s << "Sabado" << endl;
+		break;
+	default:
+		break;
+	}
+	s << "Hora: " << hora << ":00" << endl
+	<< mas->toString() 
+	<< doc->toString();
 	return s.str();
 }
